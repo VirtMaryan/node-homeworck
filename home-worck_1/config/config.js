507a2d322aa45d33368c25c0b064e4d0574a18b4
VirtMaryan <marik1488ss@gmail.com>
2022-06-02 20:48:37 +0300

    add process.env

--- home-worck_1/config/config.js
+++ home-worck_1/config/config.js
@@ -2,12 +2,12 @@ module.exports = {
   PORT: process.env.PORT || 5000,
   MONGO_DB_URL: process.env.MONGO_DB_URL || 'mongodb://localhost:27017/hebron_homeworck',
 
-  ACCESS_TOKEN: 'TOKEN_SECRET',
-  REFRESH_TOKEN: 'REFRESH_SECRET',
-  ACTION_TOKEN: 'ACTION_SECRET',
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN || 'TOKEN_SECRET',
+  REFRESH_TOKEN: process.env.REFRESH_TOKEN || 'REFRESH_SECRET',
+  ACTION_TOKEN: process.env.ACTION_TOKEN || 'ACTION_SECRET',
 
   SYSTEM_MAIL: process.env.SYSTEM_MAIL || '',
   SYSTEM_MAIL_PASSWORD: process.env.SYSTEM_MAIL_PASSWORD || '',
 
-  FRONTEND_URL: 'http://localhost:3000'
+  FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3000'
 }
